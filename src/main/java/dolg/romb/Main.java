d33594b167e592c2ab25dc908425badafe30e350
pwfszerko <bendeguzszeri@gmail.com>
2024-01-10 10:26:26 +0100

    teszt1

---
+++ src/main/java/dolg/romb/Main.java
@@ -0,0 +1,7 @@
+package dolg.rom;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/dolg/romb/romb.java
@@ -0,0 +1,7 @@
+package dolg.romb;
+
+public class romb {
+    public double calcArea(double side){
+        return 0.0;
+    }
+}
